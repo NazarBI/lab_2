e2565ea47337e001f8a6dfaf6aafe3e06105ea17
Nazar <127696088+NazarBI@users.noreply.github.com>
2023-06-05 14:56:00 +0300

    Add files via upload

---
+++ lab_2/.gitattributes.py
@@ -0,0 +1,9 @@
+ я використав команду для клонуввння git clone <https://github.com/NazarBI/lab_2.git>
+створив відгалуження (branch) та перейшов з точної master на новостворенну
+використав команди 
+git log 
+git branch 
+git checkout 
+зміни не відображаються у файлі README.md на гілці master, оскільки вони були внесені на іншу гілку new_branch.
+git add README.md
+git commit -m"lab_2"
